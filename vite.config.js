667cceb8afa79172b5ab2e26d3c024f5801beba3
Peter Key <137883736+peterkey@users.noreply.github.com>
2024-04-27 14:09:34 +0100

    Update esbuild configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/wedding/",
   plugins: [react()],
+  esbuild: {
+    loader: "jsx",
+    include: /\.jsx$/,
+    exclude: [],
+  },
 });
