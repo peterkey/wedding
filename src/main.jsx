40ad0577de5a2269ba79249c479e1687969444a9
Peter Key <137883736+peterkey@users.noreply.github.com>
2024-04-27 13:53:37 +0100

    Update service worker registration in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.unregister();
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <App />
